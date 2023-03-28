16c26fc0e091ffcb3cba393f0c8bb87574b2fc38
carlos-saiz <useles123456789@gmail.com>
2023-03-28 09:30:04 +0200

    he añadido un piano funcional con su archivo de javascript(solo funciona con el mouse)

---
+++ piano.js
@@ -0,0 +1,11 @@
+const context= new AudioContext();
+function jsNota(frecuencia){
+        let o= context.createOscillator();
+        g=context.createGain();
+        o.connect(g);
+        o.type="sawtooth";
+        o.frequency.value=frecuencia;
+        g.connect(context.destination);
+        o.start(0);
+        g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
+    }
