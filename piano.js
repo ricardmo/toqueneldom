9b9a31133d614d425d8f89113ae29d148b5cde61
carlos-saiz <useles123456789@gmail.com>
2023-03-28 13:59:59 +0200

    he añadido el piano con eventos en mouse falta arreglar teclas

--- piano.js
+++ piano.js
@@ -1,96 +1,25 @@
+const context = new AudioContext();
 
-    const context = new AudioContext();
+function playSound(audio) {
+  let sound = new Audio(audio);
+  sound.play();
+}
 
-    function jsNota(frecuencia) {
-      let o = context.createOscillator();
-      g = context.createGain();
-      o.connect(g);
-      o.type = "sawtooth";
-      o.frequency.value = frecuencia;
-      g.connect(context.destination);
-      o.start(0);
-      g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 1.5);
-    }
-    
-    document.addEventListener("keydown", function(event) {
-      switch (event.key) {
-        case "b":
-          jsNota(246.94);
-          break;
-        case "a":
-          jsNota(220);
-          break;
-        case "g":
-          jsNota(196);
-          break;
-        case "f":
-          jsNota(174.61);
-          break;
-        case "e":
-          jsNota(164.81);
-          break;
-        case "d":
-          jsNota(146.83);
-          break;
-        case "c":
-          jsNota(130.81);
-          break;
-        default:
-          break;
-      }
-    });
-    
-    document.querySelectorAll(".white-key").forEach(key => {
-      key.addEventListener("click", function() {
-        switch (key.id) {
-          case "b":
-            jsNota(246.94);
-            break;
-          case "a":
-            jsNota(220);
-            break;
-          case "g":
-            jsNota(196);
-            break;
-          case "f":
-            jsNota(174.61);
-            break;
-          case "e":
-            jsNota(164.81);
-            break;
-          case "d":
-            jsNota(146.83);
-            break;
-          case "c":
-            jsNota(130.81);
-            break;
-          default:
-            break;
-        }
-      });
-    });
-    
-    document.querySelectorAll(".black-key").forEach(key => {
-      key.addEventListener("click", function() {
-        switch (key.id) {
-          case "as":
-            jsNota(233.08);
-            break;
-          case "gs":
-            jsNota(207.65);
-            break;
-          case "fs":
-            jsNota(185);
-            break;
-          case "ds":
-            jsNota(138.59);
-            break;
-          case "cs":
-            jsNota(123.47);
-            break;
-          default:
-            break;
-        }
-      });
-    });
-    
+function jsNota(note) {
+  playSound(note.dataset.note);
+}
+
+document.addEventListener("keydown", function(event) {
+  let note = document.querySelector(`[data-key="${event.keyCode}"]`);
+  if (note) {
+    jsNota(note);
+  }
+});
+
+const keys = document.querySelectorAll('.set li');
+
+keys.forEach(function(key) {        
+  key.addEventListener('click', function() {
+    jsNota(this);
+  });
+});
