762cee2993161b9a6368b4412797f5da58ec290c
ricardmo <ricardmobcn@gmail.com>
2023-04-03 09:19:01 +0200

    he hecho la bateria

--- javascript/piano.js
+++ javascript/piano.js
@@ -1,27 +1,47 @@
-let teclaspiano = ['a' , 'w' , 'd' , 'r' , 'f' , 't' , 'g' , 'h' , 'u' , 'j' , 'i' , 'k'];
-const audiopiano = {
-  a: 'sounds/do.wav',
-  w: 'sounds/re.wav',
-  d: 'sounds/re2.wav',
-  r: 'sounds/mi.wav',
-  f: 'sounds/mi2.wav',
-  t: 'sounds/fa.wav',
-  g: 'sounds/fa2.wav',
-  h: 'sounds/sol.wav',
-  u: 'sounds/la.wav',
-  j: 'sounds/la2.wav',
-  i: 'sounds/si.wav',
-  k: 'sounds/do.wav',
-}
+ // ------------------------------------ RUTAS AUDIO --------------------------------
 
-for(let index = 0 ; index < Object.keys(audiopiano).length ; index++ ) {
-  const tecla = teclaspiano[index];
-  const archivo = audiopiano[tecla];
-  const piano = new Audio(archivo);
 
-  document.addEventListener('keypress', (e) => {
-    if(e.key === tecla){
-      piano.play();
+const rutasAudio = [
+  '/sounds/piano/do.wav',
+  '/sounds/piano/re.wav',
+  '/sounds/piano/re2.wav',
+  '/sounds/piano/mi.wav',
+  '/sounds/piano/mi2.wav',
+  '/sounds/piano/fa.wav',
+  '/sounds/piano/fa2.wav',
+  '/sounds/piano/sol.wav',
+  '/sounds/piano/la.wav',
+  '/sounds/piano/la2.wav',
+  '/sounds/piano/si.wav',
+  '/sounds/piano/do.wav',
+]
+
+ 
+ //--------------------------------------vvv TECLAS PIANO vvv------------------------------------------------------------------------
+function Pianokeyboard (){
+
+let teclasPiano = ['a' , 'w' , 'd' , 'r' , 'f' , 't' , 'g' , 'h' , 'u' , 'j' , 'i' , 'k'];
+
+  for(let index = 0 ; index < teclasPiano.length ; index++ ) {
+    const tecla = teclasPiano[index];
+  
+  //console.log(tecla , rutaAudio);
+  
+    document.addEventListener('keypress', (e) => {
+      if(e.key === tecla){
+        const rutaAudio = rutasAudio[index];
+        let audio = document.createElement('audio');
+        audio.setAttribute('src', rutaAudio);
+        //console.log(audio) crear appenChild para evento click
+        audio.play();
     }
-  })
+    })
+  }
+
+
 }
+
+Pianokeyboard();
+
+//--------------------------------------vvv CLICK PIANO vvv------------------------------------------------------------------------
+
